06ca54a0503b4d9617b51711006a5efdd20cc809
heeejinin <khin0115@gmail.com>
2024-11-29 17:22:19 +0900

    [Setting] Generated Project

---
+++ src/main/java/com/project/storereservation/StorereservationApplication.java
@@ -0,0 +1,13 @@
+package com.project.storereservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorereservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorereservationApplication.class, args);
+	}
+
+}
